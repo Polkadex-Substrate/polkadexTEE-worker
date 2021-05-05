6a9dbcb3777eac7ff6e543b9744e1e83e2a5f883
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-05-05 09:43:20 +0200

    fix library errors

--- polkadex-primitives/src/lib.rs
+++ polkadex-primitives/src/lib.rs
@@ -1,5 +1,15 @@
+#![cfg_attr(all(not(target_env = "sgx"), not(feature = "std")), no_std)]
+#![cfg_attr(target_env = "sgx", feature(rustc_private))]
+
+#[cfg(feature = "sgx")]
+use sgx_tstd as std;
+
+use std::vec::Vec;
+
 use codec::{Encode, Decode};
-use my_node_runtime::{AccountId,Header};
+use sp_core::crypto::AccountId32;
+
+pub type AccountId = AccountId32;
 
 #[derive(Encode, Decode, Clone, Debug, PartialEq)]
 pub struct LinkedAccount {
