fa488c18f0dac18861a36f4f816f4a223921c9ee
Gautham J <Gauthamastro@gmail.com>
2021-05-07 09:30:46 +0530

    Fix Clippy Issues

--- worker/src/polkadex.rs
+++ worker/src/polkadex.rs
@@ -4,11 +4,11 @@ use sp_runtime::ModuleId;
 use my_node_runtime::{AccountId, Header};
 use sp_core::sr25519;
 use sp_core::storage::StorageKey;
-use sp_runtime::MultiSignature;
 use sp_runtime::traits::{AccountIdConversion, IdentifyAccount, Verify};
+use sp_runtime::MultiSignature;
 use substrate_api_client::Api;
 
-use polkadex_primitives::{LinkedAccount,PolkadexAccount};
+use polkadex_primitives::{LinkedAccount, PolkadexAccount};
 
 pub fn get_main_accounts(header: Header, api: &Api<sr25519::Pair>) -> Vec<PolkadexAccount> {
     // Read the genesis account
@@ -16,22 +16,24 @@ pub fn get_main_accounts(header: Header, api: &Api<sr25519::Pair>) -> Vec<Polkad
 
     // Recursively get all the LinkedAccounts and Proofs ( i.e next == None)
     let mut accounts: Vec<PolkadexAccount> = vec![];
-    let last_account = get_storage_and_proof(&genesis_account_id, &header, api);
+    let mut last_account = get_storage_and_proof(&genesis_account_id, &header, api);
     accounts.push(last_account.clone());
 
     while last_account.account.next != None {
-        let last_account = get_storage_and_proof(&last_account.account.next.clone().unwrap(), &header, api);
+        last_account =
+            get_storage_and_proof(&last_account.account.next.clone().unwrap(), &header, api);
         accounts.push(last_account.clone());
     }
     accounts
 }
 
-pub fn get_storage_and_proof(acc: &AccountId, header: &Header, api: &Api<sr25519::Pair>) -> PolkadexAccount {
-    let last_acc: LinkedAccount = api.get_storage_map(
-        "OCEX",
-        "MainAccounts",
-        acc,
-        Some(header.hash()))
+pub fn get_storage_and_proof(
+    acc: &AccountId,
+    header: &Header,
+    api: &Api<sr25519::Pair>,
+) -> PolkadexAccount {
+    let last_acc: LinkedAccount = api
+        .get_storage_map("OCEX", "MainAccounts", acc, Some(header.hash()))
         .unwrap()
         .map(|account: LinkedAccount| account.into())
         .unwrap();
