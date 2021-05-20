683cb02d5a0eb8be7db8d1d17dcc62665d826da2
Felix Müller <felix.mueller@scs.ch>
2021-05-20 09:53:00 +0200

    first draft for demo script to call the polkadex gw rpf functions

---
+++ client/demo_polkadex_gw.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# setup:
+# run all on localhost:
+#   substratee-node purge-chain --dev
+#   substratee-node --tmp --dev -lruntime=debug
+#   rm chain_relay_db.bin
+#   substratee-worker init_shard
+#   substratee-worker shielding-key
+#   substratee-worker signing-key
+#   substratee-worker run
+#
+# then run this script
+
+# usage:
+#  demo_direct_call.sh <NODEPORT> <WORKERRPCPORT>
+
+# using default port if none given as arguments
+NPORT=${1:-9944}
+RPORT=${2:-2000}
+
+echo "Using node-port ${NPORT}"
+echo "Using worker-rpc-port ${RPORT}"
+echo ""
+
+CLIENT="./substratee-client -p ${NPORT} -P ${RPORT}"
+
+# does this work when multiple workers are in the registry?
+read MRENCLAVE <<< $($CLIENT list-workers | awk '/  MRENCLAVE:[[:space:]]/ { print $2 }')
+
+# example for calling 'place_order'
+echo "Alice places order with proxy account"
+$CLIENT trusted place_order //AliceIncognito //AliceIncognitoProxy \
+ --marketid=btcusd --markettype=trusted --ordertype=limit --orderside=ask --quantity=987345 \
+ --mrenclave $MRENCLAVE --direct
+
+echo -n "Get balance of Alice"
+$CLIENT trusted get_balance //AliceIncognito --tokenid=btc --mrenclave $MRENCLAVE
+
+echo "Cancel order"
+$CLIENT trusted cancel_order //AliceIncognito //AliceIncognitoProxy \
+ --marketid=btcusd --markettype=trusted --ordertype=limit --orderside=ask --quantity=987345 \
+ --mrenclave $MRENCLAVE --direct
+
+echo "Withdraw"
+$CLIENT trusted withdraw //AliceIncognito //AliceIncognitoProxy --tokenid=btc --quantity=293 \
+ --mrenclave $MRENCLAVE
