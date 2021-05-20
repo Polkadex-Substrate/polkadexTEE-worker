d9fe65f1387285cbf8aa05893512daba5335ffe2
Felix Müller <felix.mueller@scs.ch>
2021-05-20 10:31:59 +0200

    Update script to call polkadex rpc functions from client

--- client/demo_polkadex_gw.sh
+++ client/demo_polkadex_gw.sh
@@ -23,25 +23,30 @@ echo "Using node-port ${NPORT}"
 echo "Using worker-rpc-port ${RPORT}"
 echo ""
 
-CLIENT="./substratee-client -p ${NPORT} -P ${RPORT}"
+CLIENT="../bin/substratee-client -p ${NPORT} -P ${RPORT}"
+
+echo "* Query on-chain enclave registry:"
+${CLIENT} list-workers
+echo ""
 
 # does this work when multiple workers are in the registry?
 read MRENCLAVE <<< $($CLIENT list-workers | awk '/  MRENCLAVE:[[:space:]]/ { print $2 }')
 
 # example for calling 'place_order'
 echo "Alice places order with proxy account"
-$CLIENT trusted place_order //AliceIncognito //AliceIncognitoProxy \
+$CLIENT trusted place_order --accountid=//AliceIncognito --proxyaccountid=//AliceIncognitoProxy \
  --marketid=btcusd --markettype=trusted --ordertype=limit --orderside=ask --quantity=987345 \
  --mrenclave $MRENCLAVE --direct
 
-echo -n "Get balance of Alice"
-$CLIENT trusted get_balance //AliceIncognito --tokenid=btc --mrenclave $MRENCLAVE
+echo "Get balance of Alice"
+$CLIENT trusted get_balance --accountid=//AliceIncognito --tokenid=btc --mrenclave $MRENCLAVE
 
 echo "Cancel order"
-$CLIENT trusted cancel_order //AliceIncognito //AliceIncognitoProxy \
+$CLIENT trusted cancel_order --accountid=//AliceIncognito --proxyaccountid=//AliceIncognitoProxy \
  --marketid=btcusd --markettype=trusted --ordertype=limit --orderside=ask --quantity=987345 \
  --mrenclave $MRENCLAVE --direct
 
 echo "Withdraw"
-$CLIENT trusted withdraw //AliceIncognito //AliceIncognitoProxy --tokenid=btc --quantity=293 \
+$CLIENT trusted withdraw --accountid=//AliceIncognito --proxyaccountid=//AliceIncognitoProxy \
+ --tokenid=btc --quantity=293 \
  --mrenclave $MRENCLAVE
