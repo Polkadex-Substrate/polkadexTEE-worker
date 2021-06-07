6a4da6a47ff61afab5a0a401b35b19d32ae22e13
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-06-07 11:48:40 +0200

    add remove proxy cmd and nicer success msgs

--- client/src/ocex_commands.rs
+++ client/src/ocex_commands.rs
@@ -49,19 +49,21 @@ pub fn register_account_command<'a>() -> Command<'a, str> {
                 let main = crate::get_pair_from_str(matches, arg_main);
                 let account_id = sr25519_core::Pair::from(main);
                 let chain_api = chain_api.set_signer(account_id.clone());
+                let public_account_id: AccountId = account_id.public().into();
 
                 // compose the extrinsic
                 let xt: UncheckedExtrinsicV4<([u8; 2], AccountId)> = compose_extrinsic!(
                     chain_api,
                     "PolkadexOcex",
                     "register",
-                    account_id.public().into()
+                    public_account_id.clone()
                 );
 
                 let tx_hash = chain_api
                     .send_extrinsic(xt.hex_encode(), XtStatus::Finalized)
+                    .unwrap()
                     .unwrap();
-                println!("[+] TrustedOperation got finalized. Hash: {:?}\n", tx_hash);
+                println!("[+] Successfully registered new account {}. Hash: {:?}\n", public_account_id, tx_hash);
                 Ok(())
             })
 }
@@ -103,13 +105,64 @@ pub fn register_proxy_command<'a>() -> Command<'a, str> {
                 "PolkadexOcex",
                 "add_proxy",
                 main_account_id.public().into(),
-                proxy
+                proxy.clone()
             );
 
             let tx_hash = chain_api
                 .send_extrinsic(xt.hex_encode(), XtStatus::Finalized)
+                .unwrap()
                 .unwrap();
-            println!("[+] TrustedOperation got finalized. Hash: {:?}\n", tx_hash);
+            println!("[+] Successfully registered new proxy account: {}. Hash: {:?}\n", proxy, tx_hash);
+            Ok(())
+        })
+}
+
+
+pub fn remove_proxy_command<'a>() -> Command<'a, str> {
+    Command::new("remove-proxy")
+        .description("Removes a registered proxy account from the polkadex offchain registry")
+        .options(|app| {
+            app.arg(
+                Arg::with_name("main")
+                    .takes_value(true)
+                    .required(true)
+                    .value_name("SS58")
+                    .help("Sender's on-chain AccountId in ss58check format"),
+            )
+            .arg(
+                Arg::with_name("proxy")
+                    .takes_value(true)
+                    .required(true)
+                    .value_name("SS58")
+                    .help("Sender's proxy AccountId in ss58check format"),
+            )
+        })
+        .runner(move |_args: &str, matches: &ArgMatches<'_>| {
+            let chain_api = crate::get_chain_api(matches);
+
+            // get the main account /sender
+            let arg_main = matches.value_of("main").unwrap();
+            let main = crate::get_pair_from_str(matches, arg_main);
+            let main_account_id = sr25519_core::Pair::from(main);
+            let chain_api = chain_api.set_signer(main_account_id.clone());
+
+            // get the proxy account
+            let proxy = crate::get_accountid_from_str(matches.value_of("proxy").unwrap());
+
+            // compose the extrinsic
+            let xt: UncheckedExtrinsicV4<([u8; 2], AccountId, AccountId)> = compose_extrinsic!(
+                chain_api,
+                "PolkadexOcex",
+                "remove_proxy",
+                main_account_id.public().into(),
+                proxy.clone()
+            );
+
+            let tx_hash = chain_api
+                .send_extrinsic(xt.hex_encode(), XtStatus::Finalized)
+                .unwrap()
+                .unwrap();
+            println!("[+] Successfully removed proxy account: {}. Hash: {:?}\n", proxy,  tx_hash);
             Ok(())
         })
 }
