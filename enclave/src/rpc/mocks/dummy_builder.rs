c888697960b5fea01102eb8af84409681e02a391
Gautham <Gauthamastro@gmail.com>
2021-06-22 17:09:01 +0530

    feat: Relicense to GPLv3.0 (#145)

--- enclave/src/rpc/mocks/dummy_builder.rs
+++ enclave/src/rpc/mocks/dummy_builder.rs
@@ -1,6 +1,6 @@
 // This file is part of Polkadex.
 
-// Copyright (C) 2020-2021 Polkadex oü.
+// Copyright (C) 2020-2021 Polkadex oü and Supercomputing Systems AG
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
 // This program is free software: you can redistribute it and/or modify
