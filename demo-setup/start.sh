b93c8e06d7f2772e14965c8de345f695afd6d547
Gautham J <Gauthamastro@gmail.com>
2021-07-04 19:48:54 +0530

    feat: added script to start all three services

---
+++ demo-setup/start.sh
@@ -0,0 +1,14 @@
+# Script assumes that host system has go-lang installed
+# The script assumes that openfinex opens it's port on 8001
+
+git clone https://github.com/openware/barong-jwt.git
+cd barong-jwt || exit
+go run main.go -role admin
+# Load the polkadex.tar to your docker:
+docker load --input polkadex.tar
+# start docker compose
+docker-compose -f compose/upstream_compose.yml up -Vd
+# wait for ~30s and then start the services again - api and engine can only be started when the other services are running already
+sleep 30s
+docker-compose -f compose/upstream_compose.yml start
+docker-compose up
