12271d1ae9d22fa0cea352ed151480936c724e0f
Gautham J <Gauthamastro@gmail.com>
2021-07-05 20:55:33 +0530

    not completed

--- demo-setup/start.sh
+++ demo-setup/start.sh
@@ -6,9 +6,9 @@ git clone https://github.com/openware/barong-jwt.git
 cd barong-jwt || exit
 go run main.go -role admin
 # Load the polkadex.tar to your docker:
-docker load --input polkadex.tar
+docker load --input polkadex.tar || exit
 # start docker compose
-docker-compose -f compose/upstream_compose.yml up -Vd
+docker-compose -f ./compose/upstream_compose.yml up -Vd || exit
 # wait for ~30s and then start the services again - api and engine can only be started when the other services are running already
 sleep 30s
 docker-compose -f compose/upstream_compose.yml start
