521b4da9f1af067745d1178a1756eff9d46bfefb
Gautham J <Gauthamastro@gmail.com>
2021-07-05 21:38:23 +0530

    rearrange

--- demo-setup/start.sh
+++ demo-setup/start.sh
@@ -1,15 +1,21 @@
 # Script assumes that host system has go-lang installed and polkadex.tar file is present in the same directory as this script
 # The script assumes that openfinex opens it's port on 8001
+# It also assumes that config.yaml is inside config folder,
 
 
 git clone https://github.com/openware/barong-jwt.git
 cd barong-jwt || exit
-go run main.go -role admin
+go build main.go
+cd ..
+./barong-jwt/main -role admin
 # Load the polkadex.tar to your docker:
 docker load --input polkadex.tar || exit
 # start docker compose
 docker-compose -f ./compose/upstream_compose.yml up -Vd || exit
 # wait for ~30s and then start the services again - api and engine can only be started when the other services are running already
+echo "Waiting for 30s to ensure all openfinex services are running"
 sleep 30s
+echo "Starting the api and engine services"
 docker-compose -f compose/upstream_compose.yml start
+echo "Starting Polkadex Node and TEE Gateway"
 docker-compose up
