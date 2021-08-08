3e6d192cb0aab856932c313d8fca3ab5e9d883fa
zktony <zktony@protonmail.com>
2021-08-08 16:22:16 +0530

    Updated benchmarking file

--- polkadex-benchmarking/README.md
+++ polkadex-benchmarking/README.md
@@ -1,25 +1,37 @@
 Polkadex Benchmarking
 =========================
-## Install SDKMAN
+## Prerequisite
+
+### Install SDKMAN
 ```bash
 $ curl -s "https://get.sdkman.io" | bash
 $ source "$HOME/.sdkman/bin/sdkman-init.sh"
 ```
-## Install Java and Scala
+### Install Java and Scala
 ```bash
-sdk install java $(sdk list java | grep -o "8\.[0-9]*\.[0-9]*\.hs-adpt" | head -1)
-sdk install sbt
+$ sdk install java $(sdk list java | grep -o "8\.[0-9]*\.[0-9]*\.hs-adpt" | head -1)
+$ sdk install sbt
 ```
+### Create Payload file
+```bash
+$ git clone https://github.com/Polkadex-Substrate/payload-creator-polkadextee
+$ cargo run --feature encode MRENCLAVE-ID
+```
+Now copy content from ```Payload.txt``` and paste in inside ```/polkadex-benchmarking/src/test/resources/data.json```.
 
-Start SBT
----------
+## Start Benchmark
+### Start SBT
+
 ```bash
 $ sbt
 ```
 
-Run all simulations
--------------------
+###Run all simulations
 
 ```bash
 > Gatling / test
+```
+You can also modify User strength from here:-
+```scala
+  setUp(scene.inject(atOnceUsers(TOTAL_USERS)).protocols(httpProtocol))
 ```
--- polkadex-benchmarking/src/test/scala/computerdatabase/ComputerWorld.scala
+++ polkadex-benchmarking/src/test/scala/computerdatabase/ComputerWorld.scala
@@ -9,33 +9,20 @@ import io.gatling.http.Predef._
 
 class PolkadexBenchmarking extends Simulation {
 
-  val httpProtocol = http.wsBaseUrl("ws://88.198.24.21:8020")
-
-  val message = """{"jsonrpc": "2.0","method": "place_order","params": [65, 61, 211, 138, 32, 161, 21, 135, 119, 9, 73, 45, 139, 62, 46, 163, 53, 200, 61, 38, 155, 91, 91, 249, 210, 145, 60, 178, 179, 171, 71, 193, 213, 2, 1, 4, 212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44, 133, 88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125, 212, 53, 147, 199, 21, 253, 211, 28, 97, 20, 26, 189, 4, 169, 159, 214, 130, 44, 133, 88, 133, 76, 205, 227, 154, 86, 132, 231, 165, 109, 162, 125, 1, 0, 28, 116, 114, 117, 115, 116, 101, 100, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 122, 4, 79, 138, 32, 252, 120, 79, 100, 117, 247, 183, 205, 3, 67, 88, 172, 14, 16, 29, 81, 162, 198, 134, 99, 201, 64, 176, 7, 172, 38, 104, 106, 129, 107, 99, 253, 123, 151, 11, 5, 166, 131, 118, 87, 9, 212, 200, 61, 19, 190, 125, 143, 81, 72, 215, 99, 242, 189, 122, 245, 131, 34, 140],"id": 1}"""
+  val httpProtocol = http.wsBaseUrl("ws://IP:8020")
+  val message = """{"jsonrpc":"2.0","method":"place_order","params":[180,88,204,28,50,1,195,127,37,170,225,17,82,25,135,210,190,231,76,144,3,80,252,2,85,218,240,146,61,81,194,95,213,2,1,4,212,53,147,199,21,253,211,28,97,20,26,189,4,169,159,214,130,44,133,88,133,76,205,227,154,86,132,231,165,109,162,125,212,53,147,199,21,253,211,28,97,20,26,189,4,169,159,214,130,44,133,88,133,76,205,227,154,86,132,231,165,109,162,125,1,0,28,116,114,117,115,116,101,100,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,10,176,61,15,174,12,109,2,183,164,220,208,129,9,236,190,104,169,7,218,32,209,86,70,246,185,130,237,22,125,176,4,5,107,199,66,123,148,126,22,150,11,216,99,141,102,204,189,13,121,62,56,60,71,116,2,108,103,48,217,61,95,142,129],"id":1}"""
   val jsonFileFeeder = jsonFile("data.json")
 
-/*val scene = scenario("testWebSocket").feed(jsonFileFeeder)
+  val scene = scenario("testWebSocket").feed(jsonFileFeeder)
     .exec(ws("openSocket").connect("")
-      .onConnected(exec(ws("sendMessage").sendText(StringBody("""{"jsonrpc": "${jsonrpc}", "method": "${method}", "params": ${params}.toArray, "id": ${id}}"""))
-        .await(20)(ws.checkTextMessage("check1").check(regex(".*")
-          .saveAs("myMessage"))))))*/
-
-  val scene = scenario("testWebSocket")
-    .exec(ws("openSocket").connect("")
-      .onConnected(repeat(100){exec(ws("sendMessage").sendText(message)
+      .onConnected(exec(ws("sendMessage").sendText(StringBody("""{"jsonrpc": "${jsonrpc}", "method": "${method}", "params": ${params.jsonStringify()}, "id": ${id}}"""))
         .await(20)(ws.checkTextMessage("check1").check(regex(".*4,0,0,0.*")
-          .saveAs("myMessage"))))}))
-    // created custom checks for checking my response
-
-    .exec(session => session{
+          .saveAs("myMessage"))))))
+    .exec(session => session {
       println(session("myMessage").as[String])
       session
     })
-    //created the session for printing the response and type-casted it to String
-
     .exec(ws("closeConnection").close)
-  //terminating the current websocket connection
 
-  setUp(scene.inject(atOnceUsers(500)).protocols(httpProtocol))
-
+  setUp(scene.inject(atOnceUsers(1000)).protocols(httpProtocol))
 }
