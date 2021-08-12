3ed3e69ab1e1ff1605f8e0423f23fb3cdcc9ee78
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-08-12 14:20:08 +0200

    revert ipfs.rs changes

--- worker/src/ipfs.rs
+++ worker/src/ipfs.rs
@@ -1,18 +1,14 @@
 /*
     Copyright 2019 Supercomputing Systems AG
-
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
-
         http://www.apache.org/licenses/LICENSE-2.0
-
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-
 */
 
 use std::fs::File;
@@ -30,7 +26,16 @@ use log::*;
 pub type Cid = [u8; 46];
 
 #[tokio::main]
-pub async fn write_to_ipfs(client: IpfsClient, data: &'static [u8]) -> Cid {
+async fn write_to_ipfs(data: &'static [u8]) -> Cid {
+    // Creates an `IpfsClient` connected to the endpoint specified in ~/.ipfs/api.
+    // If not found, tries to connect to `localhost:5001`.
+    let client = IpfsClient::default();
+
+    match client.version().await {
+        Ok(version) => info!("version: {:?}", version.version),
+        Err(e) => eprintln!("error getting version: {}", e),
+    }
+
     let datac = Cursor::new(data);
     let (tx, rx) = channel();
 
@@ -47,15 +52,10 @@ pub async fn write_to_ipfs(client: IpfsClient, data: &'static [u8]) -> Cid {
 }
 
 #[tokio::main]
-pub async fn write_to_ipfs_default(data: &'static [u8]) -> Cid {
+pub async fn read_from_ipfs(cid: Cid) -> Result<Vec<u8>, String> {
     // Creates an `IpfsClient` connected to the endpoint specified in ~/.ipfs/api.
     // If not found, tries to connect to `localhost:5001`.
     let client = IpfsClient::default();
-    write_to_ipfs(client, data)
-}
-
-#[tokio::main]
-pub async fn read_from_ipfs(client: IpfsClient, cid: Cid) -> Result<Vec<u8>, String> {
     let h = str::from_utf8(&cid).unwrap();
 
     info!("Fetching content from: {}", h);
@@ -66,14 +66,6 @@ pub async fn read_from_ipfs(client: IpfsClient, cid: Cid) -> Result<Vec<u8>, Str
         .map_err(|e| e.to_string())
         .try_concat()
         .await
-}
-
-#[tokio::main]
-pub async fn read_from_ipfs_default(cid: Cid) -> Result<Vec<u8>, String> {
-    // Creates an `IpfsClient` connected to the endpoint specified in ~/.ipfs/api.
-    // If not found, tries to connect to `localhost:5001`.
-    let client = IpfsClient::default();
-    read_from_ipfs(client, cid)
 }
 
 #[no_mangle]
@@ -88,7 +80,7 @@ pub unsafe extern "C" fn ocall_write_ipfs(
     let state = slice::from_raw_parts(enc_state, enc_state_size as usize);
     let cid = slice::from_raw_parts_mut(cid, cid_size as usize);
 
-    let _cid = write_to_ipfs_default(state);
+    let _cid = write_to_ipfs(state);
     cid.clone_from_slice(&_cid);
     sgx_status_t::SGX_SUCCESS
 }
@@ -102,7 +94,7 @@ pub unsafe extern "C" fn ocall_read_ipfs(cid: *const u8, cid_size: u32) -> sgx_s
     let mut cid = [0; 46];
     cid.clone_from_slice(_cid);
 
-    let result = read_from_ipfs_default(cid);
+    let result = read_from_ipfs(cid);
     match result {
         Ok(res) => {
             let filename = str::from_utf8(&cid).unwrap();
