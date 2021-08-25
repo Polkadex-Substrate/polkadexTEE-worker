bc5c97fcc09b869462899927e819fcb1dce59a57
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-08-25 13:05:06 +0200

    readd constants.rs with polkadex files

---
+++ worker/src/constants.rs
@@ -0,0 +1,33 @@
+// This file is part of Polkadex.
+
+// Copyright (C) 2020-2021 Polkadex oü and Supercomputing Systems AG
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// Iterator for orderbook mirror returns these many elements in a single yield
+pub static ORDERBOOK_MIRROR_ITERATOR_YIELD_LIMIT: usize = 1000;
+
+// polkadex DB file names
+pub static DEFAULT_STORAGE_PATH: &str = "polkadex_storage";
+pub static ORDERBOOK_DISK_STORAGE_FILENAME: &str = "orderbook.bin";
+pub static NONCE_DISK_STORAGE_FILENAME: &str = "nonce.bin";
+pub static BALANCE_DISK_STORAGE_FILENAME: &str = "balance.bin";
+
+// IPFS gateway
+pub static IPFS_HOST: &str = "localhost";
+pub static IPFS_PORT: u16 = 5001;
+
+// Interval of disk snapshot
+pub static SNAPSHOT_INTERVAL: u64 = 1000; // ms
