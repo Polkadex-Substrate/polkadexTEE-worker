e0d0242bfabb641ff4f6dd8ba8b4eb1b432028d9
Bigna Härdi <bhaerdi@devsgx02.scs-ad.scs.ch>
2021-08-26 14:10:42 +0200

    adapt pallet to match new develop node

--- enclave/src/constants.rs
+++ enclave/src/constants.rs
@@ -39,7 +39,7 @@ pub static RA_API_KEY_FILE: &str = "../bin/key.txt";
 
 // you may have to update these indices upon new builds of the runtime
 // you can get the index from metadata, counting modules starting with zero
-pub static SUBSRATEE_REGISTRY_MODULE: u8 = 38u8;
+pub static SUBSRATEE_REGISTRY_MODULE: u8 = 35u8;
 pub static REGISTER_ENCLAVE: u8 = 0u8;
 //pub static UNREGISTER_ENCLAVE: u8 = 1u8;
 pub static CALL_WORKER: u8 = 2u8;
@@ -48,7 +48,7 @@ pub static BLOCK_CONFIRMED: u8 = 4u8;
 pub static SHIELD_FUNDS: u8 = 5u8;
 
 // Polkadex Module Constants ( this should be updated if order of modules in the runtime is changed
-pub static OCEX_MODULE: u8 = 39u8;
+pub static OCEX_MODULE: u8 = 36u8;
 pub static OCEX_REGISTER: u8 = 3u8;
 pub static OCEX_ADD_PROXY: u8 = 4u8;
 pub static OCEX_REMOVE_PROXY: u8 = 5u8;
