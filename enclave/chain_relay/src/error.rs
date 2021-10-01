4206cb001dab5cd58ad834d52d4d2afc1044ee9c
haerdib <73821294+haerdib@users.noreply.github.com>
2021-10-01 14:49:39 +0200

    fix invalid finality proof (#297)
    * incooparate integritee-worker commit 1a06fa456328d442ea3ce551ee7edbb820950dff
    * revert copy mistakes
    * comment failing withdraw unit test

--- enclave/chain_relay/src/error.rs
+++ enclave/chain_relay/src/error.rs
@@ -1,6 +1,5 @@
 use crate::std::string::String;
 use derive_more::{Display, From};
-use std::convert::From;
 
 /// Substrate Client error
 #[derive(Debug, Display, From)]
@@ -12,9 +11,11 @@ pub enum JustificationError {
     #[display(fmt = "bad justification for header: {}", _0)]
     #[from(ignore)]
     BadJustification(String),
+    /// Invalid authorities set received from the runtime.
+    InvalidAuthoritiesSet,
 }
 
-#[derive(Debug)]
+#[derive(Debug, From)]
 pub enum Error {
     // InvalidStorageProof,
     StorageRootMismatch,
@@ -23,17 +24,7 @@ pub enum Error {
     ValidatorSetMismatch,
     InvalidAncestryProof,
     NoSuchRelayExists,
-    InvalidFinalityProof,
+    InvalidFinalityProof(JustificationError),
     // UnknownClientError,
     HeaderAncestryMismatch,
-}
-
-impl From<JustificationError> for Error {
-    fn from(e: JustificationError) -> Self {
-        match e {
-            JustificationError::BadJustification(_) | JustificationError::JustificationDecode => {
-                Error::InvalidFinalityProof
-            }
-        }
-    }
 }
